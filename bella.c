5b0bf6631f9d826b1906b0e0d618c455ae90b76c
bellacocho123 <isabelsky59@gmail.com>
2023-11-01 09:17:26 -0700

    legal marriage 18

--- bella.c
+++ bella.c
@@ -1,58 +1,26 @@
 #include "main.h"
+#include <stdio.h>
 
-int _printf(const char *format, ...)
+/**
+ * main - show the legal age for marriage
+ *
+ * Return: 0 for success
+ */
+int main(void)
 {
+	int age;
 
-	va_list list;
-	int len;
-	int printed_chars = 0;
+	printf("Enter your age: ");
+	scanf("%d", &age);
 
-	va_start(list, format);
-
-	if (format == NULL)
-		return (-1);
-
-	while (*format)
+	if (age >= legal_marriage_age)
 	{
-	if (*format == '%' && *(format + 1) != '\0')
-	{
-		format++;
-		switch (*format)
-		{
-			case 'c':
-			{
-				char c = va_arg(list, int);
-
-				write(1, &c, 1);
-				printed_chars++;
-				break;
-			}
-			case 's':
-			{
-				char *str = va_arg(list, char *);
-
-				if (str == NULL)
-					str = "(null)";
-				len = 0; /* Initialize len here */
-				while (str[len])
-					len++;
-				write(1, str, len);
-				printed_chars += len;
-				break;
-			}
-			case '%':
-				write(1, "%", 1);
-		printed_chars += 1;
-		break;
-		}
+		printf("You are of legal age to get married.\n");
 	}
 	else
 	{
-	write(1, format, 1);
-	printed_chars++;
+		printf("You are not of legal age to get married.\n");
 	}
-	format++;
-	}
-	va_end(list);
-	return (printed_chars);
+
+	return (0);
 }
