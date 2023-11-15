4ad8d122ab9f8a1879c54cc3b0a0e9b13afb02f1
bellacocho123 <isabelsky59@gmail.com>
2023-11-15 05:47:49 -0800

    test_bella

--- test_bella.c
+++ test_bella.c
@@ -1,54 +1,16 @@
-#include "main.h"
+#include "shell.h"
 
 /**
- * _bettytest - function to print output
- * @format: input
+ * _isupper - uppercase letters
+ * @c: char to check
  *
- * Description - function to format and print output to the
- * standard output stream
- *
- * Return: 0 on success
+ * Return: 0 or 1
  */
 
-int _bettytest(const char *format, ...)
+int _isupper(int c)
 {
-	unsigned int i = 0, i_val = 0;
-
-	va_list args_bella;
-
-	va_start(args_bella, format);
-
-	for ( ; format[i] != '\0'; i++)
-	{
-		if (format[i] != '%')
-		{
-			_putchar(format[i]);
-		}
-		else if (format[i + 1] == 'c')
-		{
-			_putchar(va_arg(args_bella, int));
-  			i++;
-		}
-		else if (format[i + 1] == 's')
-		{
-			int i_value = _puts(va_arg(args_bella, char *));
-
-			i++;
-			i_val += (i_value - 1);
-		}
-		else if (format[i + 1] == '%')
-		{
-			_putchar('%');
-			i++;
-		}
-		else if ((format[i + 1] == 'd') || (format[i + 1] == 'i'))
-		{
-			_our_int(va_arg(args_bella, int));
-			i++;
-		}
-		i_val += 1;
-	}
-	va_end(args_bella);
-
-	return (i_val);
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
 }
