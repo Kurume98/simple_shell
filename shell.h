5fd2e53864f940796f58444d43b959934d6413ab
bellacocho123 <isabelsky59@gmail.com>
2023-11-16 08:37:55 -0800

    changes

--- shell.h
+++ shell.h
@@ -35,7 +35,7 @@
 #define HIST_MAX	4096
 
 extern char **environ;
-
+int my_cd(info_t *info);
 
 /**
  * struct liststr - singly linked list
@@ -95,7 +95,7 @@ typedef struct passinfo
 	int cmd_buf_type; /* CMD_type ||, &&, ; */
 	int readfd;
 	int histcount;
-} info_t;
+}info_t;
 
 #define INFO_INIT \
 {NULL, NULL, NULL, 0, 0, 0, 0, NULL, NULL, NULL, NULL, NULL, 0, 0, NULL, \
